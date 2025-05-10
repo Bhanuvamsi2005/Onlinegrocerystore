c2d01210dcdf897f160266a56de593d806d1d3d0
Bhanuvamsi2005 <bhanuvamsia@gmail.com>
2025-05-11 02:09:15 +0530

    Update AdminLogin.jsx

--- Grocerystore/src/components/AdminLogin.jsx
+++ Grocerystore/src/components/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./AdminLogin.css";
+import "./Adminlogin.css";
 
 function AdminLogin() {
   const [adminId, setAdminId] = useState("");
